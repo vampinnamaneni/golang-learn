71a95aeee17f96654979e771744f4eab8d8d434c
vampinnamaneni <vampinnamaneni@gmail.com>
2020-06-09 01:37:43 +1000

    First program fixed

--- hello-world/helloworld.go
+++ hello-world/helloworld.go
@@ -10,10 +10,16 @@ func main() {
 	for i := 0; i < 100; i++ {
 		if i%2 == 0 {
 			fmt.Println(i)
+			bar()
 		}
 	}
+
 }
 
 func foo() {
 	fmt.Println("My first Go Program")
+}
+
+func bar() {
+	fmt.Println("Exit the program")
 }
