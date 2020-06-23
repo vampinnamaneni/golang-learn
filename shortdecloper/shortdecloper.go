adef6c8a12ab5772220d24c7f781c3923d25e345
vampinnamaneni <vampinnamaneni@gmail.com>
2020-06-23 22:12:16 +1000

    Short declaration operator

---
+++ shortdecloper/shortdecloper.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+//:= is the short declaration operator which declares and assigns a value to a variable in GOlang
+
+func main() {
+	x := "02401"
+	fmt.Println(x)
+	y := "A"
+	fmt.Println(y)
+	z := "0557"
+	fmt.Println(x + y + z)
+}
